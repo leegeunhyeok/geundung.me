6176ca09a12825d4a6e966233c62f04db6054351
leegeunhyeok <lghlove0509@naver.com>
2018-12-23 23:56:47 +0900

    Add sample server

---
+++ server/main.js
@@ -0,0 +1,31 @@
+const config = require('config')
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const fs = require('fs')
+
+const dist = path.join(__dirname, '../', 'dist')
+
+app.use('/css', express.static(path.join(dist, 'css')))
+app.use('/fonts', express.static(path.join(dist, 'fonts')))
+app.use('/js', express.static(path.join(dist, 'js')))
+
+app.get('/', (req, res) => {
+  fs.readFile(path.join(dist, 'index.html'), (err, data) => {
+    if (err) {
+      console.log(err)
+      res.writeHead(500, { 'Content-Type': 'text/html' })
+      res.write(err)
+      res.end()
+      return
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(data)
+    res.end()
+  })
+})
+
+app.listen(config.get('port'), () => {
+  console.log('Server started')
+})
